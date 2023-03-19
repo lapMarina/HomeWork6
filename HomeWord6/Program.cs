cab16fbf89ce3296981a7c4a4a9710e635d2b83a
Fobos23 <gutearbeiter@gmail.com>
2023-03-19 15:39:26 +0300

    first task

--- HomeWord6/Program.cs
+++ HomeWord6/Program.cs
@@ -4,9 +4,25 @@ namespace HomeWord6
 {
     class Program
     {
+        static void FirstTask() 
+        {
+            Console.WriteLine("Введите 10 целых чисел");
+            var input_string = Console.ReadLine();
+            var separators = new char[2] { ' ', ',' };
+            var str_elements = input_string.Split(separators);
+            var count_positive_numbers = 0;
+            foreach (var element in str_elements) 
+            {
+                var number = int.Parse(element);
+                if (number > 0)
+                    count_positive_numbers++;
+            }
+            Console.WriteLine($"Количество чисел больше 0: {count_positive_numbers}");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            FirstTask();
         }
     }
 }
